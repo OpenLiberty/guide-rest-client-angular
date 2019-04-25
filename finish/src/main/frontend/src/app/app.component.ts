f0889a3f9fcf0dbfc73ae74e72db84324d0e93ed
realModusOperandi <liamwestby@icloud.com>
2019-04-25 09:09:08 -0500

    Add draft

---
+++ finish/src/main/frontend/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ArtistsService {
+  constructor(private http: HttpClient) { }
+
+  private static ARTISTS_URL = 'http://localhost:9080/artists';
+
+  async fetchArtists() {
+    try {
+      let data: any = await this.http.get(ArtistsService.ARTISTS_URL).toPromise();
+      return data;
+    } catch (error) {
+      console.error(`Error occurred: ${error}`);
+    }
+  }
+
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  providers: [ ArtistsService ],
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  artists: any[] = [];
+
+  constructor(private artistsService: ArtistsService) { }
+
+  ngOnInit() {
+    this.artistsService.fetchArtists().then(data => {
+      this.artists = data;
+    });
+  }
+
+
+}
